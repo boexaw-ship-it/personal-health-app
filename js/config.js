0e2a48712248871acf4286bfb1432b1ae9a0e349
zawzaw <boexaw@gmail.com>
2026-07-09 14:35:27 +0630

    Modify Supabase API key format in config.js
    Updated the Supabase API key format in config.js.

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-// Supabase Project API Settings
+// Supabase Project API Settings (2026 New API Key Format)
 const SUPABASE_URL = "https://kcvbhakbspmrlacookki.supabase.co";
-const SUPABASE_ANON_KEY = "Sb_publishable_P3IVSaoF_5btHBl1F9ASZA_EzNT6xw2";
+const SUPABASE_ANON_KEY = "sb_publishable_P3IVSaoF_5btHBl1F9ASZA_EzNT6xw2";
